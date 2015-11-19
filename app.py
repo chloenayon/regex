dddb77fd1c8c4126462d794eda725dec44618979
Chloe Kaubisch <nemononiam@gmail.com>
2015-11-19 11:46:55 -0500

    updates

--- app.py
+++ app.py
@@ -8,4 +8,11 @@ def start():
     if request.method = "GET":
         return render_template("home.html")
     else:
+        question = request.form['query']
+        print question
         return render_template("result.html")
+    
+
+if __name__=="__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
