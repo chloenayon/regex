3087f359825d36841f22c432c3b68f637da3e50e
Randolph Higgins <higginsrandolph78@gmail.com>
2015-11-18 19:34:09 -0500

    the back end is working as long as all the pages load

--- utils.py
+++ utils.py
@@ -1,42 +1,45 @@
 import urllib2, google, bs4, re, math
 
 def parse_names(text):
-    more = True
-    list_of_names = []
-    while(more):
-        name = re.match('[A-Z][a-z]+ [A-Z][a-z]+', text)
-        re.sub('[A-Z][a-z]{1,15} [A-Z][a-z]{1,15} ','name',text)
-        if(name == None):
-            more = False
-        else:
-            list_of_names.append(name)
-            print list_of_names[0]
+    list_of_names = re.findall('[A-Z][a-z]+ [A-Z][a-z]+', text)
     return list_of_names
 
 def top_names(who_query):
-    pages = 10
+    pages = 6
+    print who_query
     urls = google.search(who_query,num=pages,start=0,stop=pages)
     result=[]
     for thing in urls:
         result.append(thing)
-    print result
     names={}
-    print result
     counter = 0
     for thing in result:
-        print counter
-        u = urllib2.urlopen(thing)
-        page = u.read()
-        soup = bs4.BeautifulSoup(page,'html')
-        raw = soup.get_text()
-        possibles = parse_names(raw)
-        power = math.log(pages, 2)-math.log(counter+1, 2)
-        counter+=1
-        for name in possibles:
-            if name not in names:
-                names[name] = power
-            else:
-                names[name]+=power
-    print names
+        if counter < 10:
+            print counter
+            u = urllib2.urlopen(thing)
+            page = u.read()
+            soup = bs4.BeautifulSoup(page,'html')
+            raw = soup.get_text()
+            possibles = parse_names(raw)
+            power = int(math.log(pages, 2))-int(math.log(counter+1, 2))+1
+            counter+=1
+            for name in possibles:
+                if name not in names:
+                    names[name] = power
+                else:
+                    names[name]+=power
+    i = 0
+    top={}
+    while i < 1:
+        most_mentioned=None
+        mentions = 0
+        for name in names:
+            if names[name] > mentions:
+                most_mentioned = name
+                mentions = names[name]
+        names[most_mentioned]=0
+        top[most_mentioned]=mentions
+        i+=1
+    print top
 
-top_names("who is president")
+top_names("who is Champ?")
