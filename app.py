311baf7f7a575370bf70a11aac12b0746a82b20e
Chloe Kaubisch <nemononiam@gmail.com>
2015-11-20 10:10:04 -0500

    broken a little

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-import flask from Flask, render_template, request
+from flask import Flask, render_template, request
 import google
+import utils
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def start():
-    if request.method = "GET":
+    if request.method == "GET":
         return render_template("home.html")
     else:
         question = request.form['query']
-        print question
-        return render_template("result.html")
+        r = utils.top_names(question)
+        return render_template("result.html", r = r)
     
 
 if __name__=="__main__":
